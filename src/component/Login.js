b907d8fe7b02ca14eca4940acc263d0b4b61db91
Mohammedily <m9741305@gmail.com>
2023-05-09 08:52:55 +0530

    safazzxdaa

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,27 +1,54 @@
 import React, {useState} from 'react';
 import "./Register.css";
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
 
+   const successnotify = (msg) => {
+    toast.success(msg, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      })
+   }
 
+   const errornotify = (msg) => {
+    toast.error(msg, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      });
+   }
 
     const handleSubmit = async(e) => {
         e.preventDefault();
         try {
-            const url = "https://asss-8gk0.onrender.com/signin";
-          const { data: res } = await axios.post(url, {email, password});
+             const url = "https://asss-8gk0.onrender.com/signin";
+             const { data: res } = await axios.post(url, {email, password});
              localStorage.setItem("data", res.data);
-                window.location.href="/home"
+             successnotify(res.message);
+             window.location.href="/home"
         } catch (error) {
-           
-           console.log(error)
+            errornotify(error.response.data.message)
+            console.log(error.response.data.message)
         }
-      
-        
+    
        
     }
 
@@ -32,10 +59,22 @@ function Login() {
             <h4 className='register2'>Login</h4>
             <input className='col-md-4 input' type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='user@gmail.com'  required/>
             <input className='col-md-4 input' type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='********'  required/>
-            <p className='input'>Need An Account <a href="/signup">Register</a></p> 
+            <p className='input'>Need An Account? <a href="/signup">Register</a></p> 
              <button className='butt' type="submit">Submit</button>
             </form>
         </div>
+        <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="colored"
+/>
     </div>
   )
 }
--- src/component/Register.js
+++ src/component/Register.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import "./Register.css";
 import axios from "axios";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
   
 
 function Register() {
@@ -10,6 +11,31 @@ function Register() {
     const [password, setPassword] = useState("");
 
 
+    const successnotify = (msg) => {
+      toast.success(msg, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        })
+     }
+  
+     const errornotify = (msg) => {
+      toast.error(msg, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
+     }
 
 
     const handleSubmit = async(e) => {
@@ -17,9 +43,10 @@ function Register() {
         try {
             const url = "https://asss-8gk0.onrender.com/signup";
           const { data: res } = await axios.post(url, {email, password});
-                window.location.href="/"
+          successnotify(res.message);
+          window.location.href="/"
         } catch (error) {
-           
+          errornotify(error.response.data.message)
            console.log(error)
         }
       
@@ -34,10 +61,22 @@ function Register() {
             <h4 className='register2'>Register</h4>
             <input className='col-md-4 input' type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='user@gmail.com'  required/>
             <input className='col-md-4 input' type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='********'  required/>
-        <p className='input'>Already Register <a href="/">Login</a></p>    
+        <p className='input'>Already Register? <a href="/">Login</a></p>    
              <button className='butt' type="submit">Submit</button>
             </form>
         </div>
+        <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="colored"
+/>
     </div>
   )
 }
